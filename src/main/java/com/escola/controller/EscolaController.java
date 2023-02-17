32f42241efdd5ad5c2c2b3bc692bdb90c049963e
˜codebyedu˜ <epereira607@gmail.com>
2023-02-17 16:00:46 -0300

    alterado retorno para response entity

--- src/main/java/com/escola/controller/EscolaController.java
+++ src/main/java/com/escola/controller/EscolaController.java
@@ -1,27 +1,28 @@
-package com.escola.controller; 
+package com.escola.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escola.business.EscolaFacade;
+import com.escola.business.ProfessorInvalidoException;
 import com.escola.model.AlunoVO;
-
-
-
 
 @RestController
 public class EscolaController {
   @GetMapping("/all")
-  public List<AlunoVO> getAlunosPorAula(@RequestParam Integer id) {
+  public ResponseEntity<List<AlunoVO>> getAlunosPorTurma(@RequestParam Integer idTurma, @RequestParam Integer idProf) {
     EscolaFacade facade = new EscolaFacade();
+    try {
+     
+      return ResponseEntity.ok().body(facade.getAlunosPorTurma(idTurma, idProf));
+    } catch (ProfessorInvalidoException e) {
+      return ResponseEntity.notFound().build();
+    }
 
-    List<AlunoVO> alunos = facade.getAlunosPorAula(id);
-
-    return alunos;
   }
-
 
 }
