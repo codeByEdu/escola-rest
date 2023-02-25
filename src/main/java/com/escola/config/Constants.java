b7cfaa7ebeb053b7adcd572abf767490a8b2c6b6
˜codebyedu˜ <epereira607@gmail.com>
2023-02-25 01:35:12 -0300

    Criado constants para retornar ao usuario

---
+++ src/main/java/com/escola/config/Constants.java
@@ -0,0 +1,6 @@
+package com.escola.config;
+
+public class Constants {
+    public static final String ALUNO_NAO_ENCONTRADO_TEXTO = "CODIGO_ALUNO_INVALIDO"; 
+    public static final String PROFESSOR_NAO_ENCONTRADO_TEXTO = "CODIGO_PROFESSOR_INVALIDO"; 
+}
