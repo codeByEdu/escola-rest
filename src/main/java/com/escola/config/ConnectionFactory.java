06c355603381afc4ad9b41dc491a69049962c93b
codeByEdu <epereira607@gmail.com>
2023-03-27 15:27:00 -0300

    organizado users com seguranca

--- src/main/java/com/escola/config/ConnectionFactory.java
+++ src/main/java/com/escola/config/ConnectionFactory.java
@@ -1,14 +1,35 @@
 package com.escola.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
-    public static Connection getConnection() {
+    static String jdbcUrl;
+    static String jdbcUser;
+    static String jdbcPassword;
+
+    public static Connection getConnection() throws IOException {
+
         try {
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost/escola", "root", "");
+            String resourceName = "application.properties"; // could also be a constant
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            Properties props = new Properties();
+            try (InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+                props.load(resourceStream);
+                jdbcUrl = props.getProperty("spring.datasource.url");
+                jdbcUser = props.getProperty("spring.datasource.user");
+                jdbcPassword = props.getProperty("spring.datasource.password");
+
+                // get the property value and print it out
+
+                return DriverManager.getConnection(
+                        jdbcUrl, jdbcUser, jdbcPassword);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
